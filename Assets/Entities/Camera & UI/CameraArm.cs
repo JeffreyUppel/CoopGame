0e2285084225fec5137cdcb27451b64a2d5b07f6
Jeffrey <Jeffrey.Uppelschoten@gmail.com>
2018-12-23 17:13:07 +0800

    added standard assets and added the camera arm

---
+++ Assets/Entities/Camera & UI/CameraArm.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraArm : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.transform.position;
+    }
+}
