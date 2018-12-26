ad8bfc7a20fdacf3b1d279c4891577ac25260d73
Jeffrey <>
2018-12-26 22:04:38 +0800

    Tweaking the character controller

---
+++ Assets/Character.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    bool isGrounded = false;
+
+    Vector3 groundNormal;
+
+    Rigidbody rigidbody;
+
+    public float jumpPower = 10f;
+    public float moveSpeed = 10f;
+    private float groundCheckDistance = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 move, bool jump)
+    {
+
+        // convert the world relative moveInput vector into a local-relative
+        // turn amount and forward amount required to head in the desired
+        // direction.
+        if (move.magnitude > 1f) move.Normalize();
+
+        CheckGroundStatus();
+        move = Vector3.ProjectOnPlane(move, groundNormal);
+        //turnAmount = Mathf.Atan2(move.x, move.z);
+        //forwardAmount = move.z;
+
+        //ApplyExtraTurnRotation();
+
+        // control and velocity handling is different when grounded and airborne:
+        if (isGrounded)
+        {
+            HandleGroundedMovement(move, jump);
+        }
+        else
+        {
+            HandleAirborneMovement();
+        }
+        // send input and other state parameters to the animator
+        UpdateAnimator(move);
+    }
+
+    void HandleGroundedMovement(Vector3 move, bool jump)
+    {
+
+        Vector3 playerMove = move * moveSpeed * Time.deltaTime;
+        rigidbody.velocity = new Vector3(playerMove.x * moveSpeed, 0, playerMove.z * moveSpeed);
+        //Vector3 dir = move - this.transform.position;
+        move.y = 0;
+        transform.LookAt(move);
+    }
+
+    void HandleAirborneMovement()
+    {
+
+    }
+
+    void UpdateAnimator(Vector3 move)
+    {
+
+    }
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, groundCheckDistance))
+        {
+            groundNormal = hitInfo.normal;
+            isGrounded = true;
+
+        }
+        else
+        {
+            isGrounded = false;
+            groundNormal = Vector3.up;
+
+        }
+    }
+}
---
+++ Assets/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserController : MonoBehaviour
+{
+    private bool isJumping = false;
+    private Vector3 move; 
+
+    [SerializeField] private bool useController = false;
+
+    private Character character;
+
+    private void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+    private void Update()
+    {
+        if (!isJumping)
+        {
+            isJumping = Input.GetButtonDown("Jump");
+        }
+
+        if (useController)
+        {
+            float xAim = Input.GetAxisRaw("Controller Right Horizontal");
+            float zAim = Input.GetAxisRaw("Controller Right Vertical");
+            Vector3 aimDirection = new Vector3(xAim, 0, zAim);
+        }
+        else
+        {
+            //Using mouse
+            Vector3 aimDirection = Input.mousePosition - this.transform.position;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        // read inputs
+        float xSpeed = Input.GetAxis("Horizontal");
+        float zSpeed = Input.GetAxis("Vertical");
+
+        // calculate move direction to pass to character
+        move = new Vector3(xSpeed, 0, zSpeed);
+
+        character.Move(move, isJumping);
+        isJumping = false;
+    }
+
+}
